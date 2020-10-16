8b61dbeeb3044cff2f667820d51c078f108d30e5
Shubham Sengar <shubhamsengar11@Shubhams-MacBook-Pro.local>
2020-10-16 12:45:32 -0700

    registering Campaign

--- WeFixIt/WeFixIt/settings.py
+++ WeFixIt/WeFixIt/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'adAPI.apps.AdapiConfig',
+    'rest_framework'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
