70e38b08671dc92ef5534980da237d1094e7d4f4
Shubham Sengar <shubhamsengar11@Shubhams-MacBook-Pro.local>
2020-10-16 13:14:33 -0700

    fixed installed apps rest_framework duplicate

--- WeFixIt/WeFixIt/settings.py
+++ WeFixIt/WeFixIt/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'rest_framework',
     'adAPI.apps.AdapiConfig',
-    'rest_framework'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
