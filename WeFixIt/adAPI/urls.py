98bcfd1e75637564e3e3a3b3e283ca4781617dd0
John <johnmanardiii@gmail.com>
2020-10-15 22:35:01 -0700

    Finished Medium Tutorial after meeting.

---
+++ WeFixIt/adAPI/urls.py
@@ -0,0 +1,16 @@
+# adAPI/urls.py
+
+from django.urls import include, path
+from rest_framework import routers
+from . import views
+
+router = routers.DefaultRouter()
+router.register(r'campaigns', views.CampaignViewSet)
+
+# Wire up API using automatic URL routing
+# include login URLs
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls',
+        namespace='rest_framework'))
+]
