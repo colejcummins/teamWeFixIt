80f75d702375ade1c6bffb0ed699bb81a74e9cfb
Austin Bryant <austindbryant@gmail.com>
2020-11-10 14:52:31 -0800

    added getad url to reeturn random ad

--- WeFixIt/adAPI/urls.py
+++ WeFixIt/adAPI/urls.py
@@ -9,11 +9,11 @@ router = routers.DefaultRouter()
 router.register(r'campaigns', views.CampaignViewSet)
 router.register(r'advertisements', views.AdvertisementViewSet)
 
-
 # Wire up API using automatic URL routing
 # include login URLs
 urlpatterns = [
     path('', include(router.urls)),
+    path('getad/', views.getad),
     path('api-auth/', include('rest_framework.urls',
                               namespace='rest_framework')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- WeFixIt/adAPI/views.py
+++ WeFixIt/adAPI/views.py
@@ -2,12 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAdminUser, AllowAny
+from rest_framework.response import Response
 from .serializers import CampaignSerializer, AdvertisementSerializer
 from .models import Campaign, Advertisement
 
 
 class CampaignViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAdminUser,)
     """
     Class that represents the page returned when /campaigns/ is accessed
 
@@ -18,6 +22,7 @@ class CampaignViewSet(viewsets.ModelViewSet):
 
 
 class AdvertisementViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAdminUser,)
     """
     Class that represents the page returned when /advertisements/ is accessed
 
@@ -25,3 +30,14 @@ class AdvertisementViewSet(viewsets.ModelViewSet):
     """
     queryset = Advertisement.objects.all().order_by('id')
     serializer_class = AdvertisementSerializer
+
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def getad(request):
+    """
+    Function that randomly returns a single ad in the database.
+    """
+    ads = Advertisement.objects.all().order_by('?').first()
+    serializer = AdvertisementSerializer(ads, many=False)
+    return Response(serializer.data)
