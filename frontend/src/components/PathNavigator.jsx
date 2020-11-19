e04d4d4325a920f790ae7395bc1791e62fd9db4b
Cole Cummins <colejcummins@gmail.com>
2020-11-19 13:20:08 -0800

    fix scrolling

--- frontend/src/components/PathNavigator.jsx
+++ frontend/src/components/PathNavigator.jsx
@@ -4,9 +4,20 @@ import _ from 'lodash';
 
 import {ReactComponent as ChevronRight} from '../chevron-right.svg';
 
+const TopContainer = styled.div`
+  display: flex;
+  width: 100%;
+
+  height: 44px;
+
+  background-color: #2B3032;
+`;
+
 const PathContainer = styled.div`
   display: flex;
-  margin: 10px 0px;
+
+  position: fixed;
+  margin: 10px 22px;
 `;
 
 const PathSpan = styled.span`
@@ -30,14 +41,16 @@ export default function PathNavigator({ navPath, setPath }) {
   path.unshift("Home");
 
   return (
-    <PathContainer>
-      {_.map(path, (val, ind) => (
-        <PathSpan>
-          <PathText onClick={() => setPath(_.slice(navPath, 0, ind))}>{val}</PathText>
-          {(ind < (path.length - 1)) ? (<ChevronRight style={{color: '#2176FF', padding: '0px 5px'}}/>) : (<div/>)}
-        </PathSpan>
-      ))}
-    </PathContainer>
+    <TopContainer>
+      <PathContainer>
+        {_.map(path, (val, ind) => (
+          <PathSpan>
+            <PathText onClick={() => setPath(_.slice(navPath, 0, ind))}>{val}</PathText>
+            {(ind < (path.length - 1)) ? (<ChevronRight style={{color: '#2176FF', padding: '0px 5px'}}/>) : (<div/>)}
+          </PathSpan>
+        ))}
+      </PathContainer>
+    </TopContainer>
   );
 
 }
