48516ca5039dc2432ae087bd9221b22e9f8aac64
Fiona Soetrisno <fiona.soetrisno@gmail.com>
2020-11-25 01:27:19 -0800

    fixed spacing again

--- frontend/src/components/AdCard.jsx
+++ frontend/src/components/AdCard.jsx
@@ -4,22 +4,22 @@ import _ from 'lodash';
 import adImage from '../images/testAdImage.png' // test image
 
 const AdCardContainer = styled.span`
-	background-color: #daefff;
-	display: inline-block;
-	width: 300px;
-	height: 300px;
-	margin: 20px;
-	border-radius: 8px;
+  background-color: #daefff;
+  display: inline-block;
+  width: 300px;
+  height: 300px;
+  margin: 20px;
+  border-radius: 8px;
 `;
 
 const AdImg = styled.img`
-	width: 200px;
+  width: 200px;
 `;
 
 const AdTitle = styled.div`
-	font-weight: 600;
-	font-size: 20px;
-	padding-top: 20px;
+  font-weight: 600;
+  font-size: 20px;
+  padding-top: 20px;
 `;
 
 const AdText = styled.div`
@@ -33,11 +33,11 @@ const AdText = styled.div`
 
 export default function AdCard({ title, description }) {
 
-	return (
-		<AdCardContainer>
-			<AdTitle>{title}</AdTitle>
-			<AdImg src={adImage}></AdImg>
-			<AdText> {description} </AdText>
-		</AdCardContainer>
-	)
+  return (
+    <AdCardContainer>
+      <AdTitle>{title}</AdTitle>
+      <AdImg src={adImage}></AdImg>
+      <AdText> {description} </AdText>
+    </AdCardContainer>
+  )
 }
