9c3b8aa74c5822c2ff7b40f480c1b85f6302b6fb
Michael Moschitto <michaelmoschitto@gmail.com>
2020-12-03 15:48:06 -0800

    commented out try except used for patch

--- backend/WeFixIt/adAPI/serializers.py
+++ backend/WeFixIt/adAPI/serializers.py
@@ -27,11 +27,11 @@ class CampaignSerializer(CountryFieldMixin, serializers.ModelSerializer):
 
         Throws a serializers. ValidationError if data is incorrectly entered.
         """
-        try:
-            if data['start_date'] > data['end_date']:
-                raise serializers.ValidationError("End date comes before start date in campaign.")
-        except serializers.ValidationError:
-            print("Request body missing date fields, most likely a patch editing name only")
+        # try:
+        if data['start_date'] > data['end_date']:
+            raise serializers.ValidationError("End date comes before start date in campaign.")
+        # except serializers.ValidationError:
+        #     print("Request body missing date fields, most likely a patch editing name only")
 
         #if Campaign.objects.filter(name=data['name']).count() != 0:
         #    raise serializers.ValidationError("Campaigns must have unique names,", data['name'], "already exists.")
