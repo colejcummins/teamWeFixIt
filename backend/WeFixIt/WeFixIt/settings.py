a9ae9f8a4a9711b8de589008ce7173bc70b3a656
Austin Bryant <austindbryant@gmail.com>
2020-12-03 22:00:12 -0800

    cors stuff

--- backend/WeFixIt/WeFixIt/settings.py
+++ backend/WeFixIt/WeFixIt/settings.py
@@ -51,6 +51,7 @@ REST_FRAMEWORK = {
 INSTALLED_APPS = [
     'rest_framework',
     'django_countries',
+    'corsheaders',
     'adAPI.apps.AdapiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -68,6 +69,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'WeFixIt.urls'
@@ -139,3 +141,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# CORS
+CORS_ORIGIN_ALLOW_ALL = True
