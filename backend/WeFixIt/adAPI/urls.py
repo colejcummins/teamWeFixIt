d36cf0d03931285650cb8cfd5196491af5934be8
Michael Moschitto <michaelmoschitto@gmail.com>
2020-12-04 01:28:00 -0800

    fixed basic host not loading I think

--- backend/WeFixIt/adAPI/urls.py
+++ backend/WeFixIt/adAPI/urls.py
@@ -3,12 +3,16 @@ from django.urls import include, path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
+from django.conf.urls import url
+from django.contrib.staticfiles.storage import staticfiles_storage
+
 
 
 # Wire up API using automatic URL routing
 # Note: THIS CODE IS NOT TO BE USED IN A PRODUCTION ENVIRONMENT
 # STATIC FILES ARE VERY INEFFICIENT AND OFTEN INSECURE.
-
+favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
 urlpatterns = [
     path('campaigns/', views.CampaignList.as_view()),
     path('campaigns/<int:pk>', views.CampaignDetail.as_view()),
@@ -24,6 +28,7 @@ urlpatterns = [
 
     path('nuke/', views.nuke),
 
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('static/images/favicon.ico'))),
     path('api-auth/', include('rest_framework.urls',
                               namespace='rest_framework')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
