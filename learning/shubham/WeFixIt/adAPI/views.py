1d1cff5a242a83ec96bdd9b4d6678f010f128457
Shubham Sengar <shubhamsengar11@Shubhams-MacBook-Pro.local>
2020-12-04 11:06:28 -0800

    pushing django tutorial

---
+++ learning/shubham/WeFixIt/adAPI/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from rest_framework import viewsets
+
+from .serializers import CampaignSerializer
+from .models import Campaign
+
+class CampaignViewSet(viewsets.ModelViewSet):
+    queryset = Campaign.objects.all().order_by('name')
+    serializer_class = CampaignSerializer
